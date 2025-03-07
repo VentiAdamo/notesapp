ee2f8b1688019ab349261c4cba77cc5978ccff97
VentiAdamo <56836457+VentiAdamo@users.noreply.github.com>
2025-03-06 22:25:31 -0500

    revert back. it is ugly

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  return ("Hello from Amplify", reactLogo);
+  return ("Hello from Amplify");
 }
 
 export default App;
