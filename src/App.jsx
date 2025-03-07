49aa4121196c1fde79d3e7c57ee47c62dddc4b4f
VentiAdamo <56836457+VentiAdamo@users.noreply.github.com>
2025-03-06 22:21:36 -0500

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  return ("Hello from Amplify");
+  return ("Hello from Amplify", reactLogo);
 }
 
 export default App;
