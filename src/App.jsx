f20a7b68038eab5338a60feb99b5bdfd02f4a301
VentiAdamo <56836457+VentiAdamo@users.noreply.github.com>
2025-03-07 19:26:29 -0500

    added authentication scripts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,123 @@
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useState, useEffect } from "react";
+import {
+  Authenticator,
+  Button,
+  Text,
+  TextField,
+  Heading,
+  Flex,
+  View,
+  Image,
+  Grid,
+  Divider,
+} from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import { getUrl, uploadData } from "aws-amplify/storage";
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
+/**
+ * @type {import('aws-amplify/data').Client}
+ */
 
-function App() {
-  return ("Hello from Amplify");
+Amplify.configure(outputs);
+const client = generateClient({
+  authMode: "userPool",
+});
+
+export default function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const { data: notes } = await client.models.Note.list();
+    await Promise.all(
+      notes.map(async (note) => {
+        if (note.image) {
+          const linkToStorageFile = await getUrl({
+            path: ({ identityId }) => `media/${identityId}/${note.image}`,
+          });
+          console.log(linkToStorageFile.url);
+          note.image = linkToStorageFile.url;
+        }
+        return note;
+      })
+    );
+    console.log(notes);
+    setNotes(notes);
+  }
+
+  async function createNote(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    console.log(form.get("image").name);
+
+    const { data: newNote } = await client.models.Note.create({
+      name: form.get("name"),
+      description: form.get("description"),
+      image: form.get("image").name,
+    });
+
+    console.log(newNote);
+    if (newNote.image) {
+      await uploadData({
+        path: ({ identityId }) => `media/${identityId}/${newNote.image}`,
+        data: form.get("image"),
+      }).result;
+    }
+
+    fetchNotes();
+    event.target.reset();
+  }
+
+  async function deleteNote({ id }) {
+    const toBeDeletedNote = {
+      id: id,
+    };
+
+    const { data: deletedNote } = await client.models.Note.delete(
+      toBeDeletedNote
+    );
+    console.log(deletedNote);
+
+    fetchNotes();
+  }
+
+  return (
+    <Authenticator>
+      {({ signOut }) => (
+        <View>
+          <Heading level={1}>My Notes App</Heading>
+
+          <Flex direction="column" alignItems="center" margin="20px">
+            <form onSubmit={createNote}>
+              <TextField name="name" label="Note Name" required />
+              <TextField name="description" label="Description" required />
+              <input type="file" name="image" />
+              <Button type="submit">Create Note</Button>
+            </form>
+          </Flex>
+
+          <Divider />
+
+          <Heading level={2}>Current Notes</Heading>
+          <Grid templateColumns="repeat(auto-fill, minmax(250px, 1fr))" gap={4}>
+            {notes.map((note) => (
+              <View key={note.id} border="1px solid #ccc" padding="10px">
+                <Text>{note.name}</Text>
+                <Text>{note.description}</Text>
+                {note.image && <Image src={note.image} alt={note.name} />}
+                <Button onClick={() => deleteNote(note)}>Delete note</Button>
+              </View>
+            ))}
+          </Grid>
+
+          <Button onClick={signOut}>Sign Out</Button>
+        </View>
+      )}
+    </Authenticator>
+  );
 }
-
-export default App;
